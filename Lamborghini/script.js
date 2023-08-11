1baf9965a32d8c8bf51c1beede8cbe39c58ed236
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-08-11 15:39:07 +0500

    Moving Lamborghini

---
+++ Lamborghini/script.js
@@ -0,0 +1,5 @@
+let aud = document.createElement('audio');
+aud.setAttribute('src', './lamborghini/sound.mp3');
+aud.setAttribute('autoplay', './lamborghini/sound.mp3');
+aud.loop = true;
+aud.play();
